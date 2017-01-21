769b95a4975827285d76dd8920eae860d45acf37
Khanh Duc Tran <ktran035@uw.edu>
2017-01-21 00:49:09 -0800

    added picture showing 'finding best lambda' 'residual sum square on test and training data' 'regularization_path'

--- crime_est.py
+++ crime_est.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def lasso_solver(lambda_, X, vec_y):
     converge = 10
@@ -41,18 +43,70 @@ def square_err(coeff, feas_rates, actual_rate):
     error_vec = actual_rate - model_rate
     error_sqr = np.sum(np.square(error_vec))
 
-    print(error_sqr)
+    return error_sqr
 
-def main():
-    df_train = pd.read_table("crime-train.txt")
-    df_test = pd.read_table("crime-test.txt")
-    arr_train = df_train.values
-    data_rows, data_columns = np.shape(arr_train)
-    feas_rates = arr_train[:data_rows, 1:data_columns]
-    crime_rates = arr_train[:data_rows, :1]
-    #print(crime_rates)
-    coeff = lasso_solver(600, feas_rates, crime_rates)
-    #print(coeff)
-    square_err(coeff, feas_rates, crime_rates)
 
-main()
+df_train = pd.read_table("crime-train.txt")
+df_test = pd.read_table("crime-test.txt")
+arr_train = df_train.values
+arr_test = df_test.values
+
+data_rows, data_columns = np.shape(arr_train)
+data_rows_test, data_columns_test = np.shape(arr_test)
+feas_rates = arr_train[:data_rows, 1:data_columns]
+crime_rates = arr_train[:data_rows, :1]
+feas_rates_test = arr_test[:data_rows_test, 1:data_columns_test]
+crime_rates_test = arr_test[:data_rows_test, :1]
+lambda_ = 600
+
+age = df_train.columns.get_loc("agePct12t29")
+pctWS = df_train.columns.get_loc("pctWSocSec")
+pctKids = df_train.columns.get_loc("PctKids2Par")
+pctIll = df_train.columns.get_loc("PctIlleg")
+housVaca = df_train.columns.get_loc("HousVacant")
+five_feas = [age, pctWS, pctKids, pctIll, housVaca]
+
+
+while(lambda_ > 1):
+    #coeff = lasso_solver(np.log(lambda_), feas_rates, crime_rates)
+    # 5.2
+    #
+    # shape = ["ro","y^","bs","gp","g*"]
+    # for i in range (len(five_feas)):
+    #     plt.plot(np.log(lambda_) ,coeff[five_feas[i]], shape[i])
+
+    # 5.3
+    # RSS = square_err(coeff, feas_rates, crime_rates)
+    # plt.plot(np.log(lambda_), RSS, "ro")
+    # lambda_ /= 2
+
+    # 5.4
+    # RSS = square_err(coeff, feas_rates_test, crime_rates_test)
+    # plt.plot(np.log(lambda_), RSS, "ro")
+    # lambda_ /= 2
+
+    # 5.5
+    # coeff = lasso_solver(lambda_, feas_rates, crime_rates)
+    # rows, columns = np.shape(np.nonzero(coeff))
+    # plt.plot(lambda_, columns, "ro")
+    # lambda_ /= 2
+
+    # 5.7
+    # coeff = lasso_solver(lambda_, feas_rates, crime_rates)
+    # RSS = square_err(coeff, feas_rates_test, crime_rates_test)
+    # plt.plot(lambda_, RSS, "ro")
+     lambda_ /= 2
+
+# 5.7 (continue)
+# The best performance lambda is approximately around 20
+best_lambda = 18
+best_coeff = lasso_solver(best_lambda, feas_rates_test, crime_rates_test)
+pos_highest_var_index = np.argmax(best_coeff)
+neg_lowest_var_index = np.argmin(best_coeff)
+print(pos_highest_var_index)
+print(neg_lowest_var_index)
+print(df_test.columns[pos_highest_var_index])
+print(df_test.columns[neg_lowest_var_index])
+
+#plt.plot(,)
+plt.show()
